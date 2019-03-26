e9f263cc5815825bb185169b79af94cb1d3d0905
Jaredana <jaredana@pdx.edu>
2019-03-25 20:57:56 -0700

    connected to mongo and added auth functionality

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var AuthController = require('./auth/AuthController');
 var users = require('./routes/users');
+var login = require('./routes/login');
+var db = require('./db');
 
 var app = express();
 
@@ -11,6 +13,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
+//urls must have /api/ in front due to proxy config'd in angular app(to deal with CORS)
 app.use('/api/v1/users', users);
+app.use('/api/v1/login', login);
+
+app.use('/api/auth', AuthController);
 
 module.exports = app;
---
+++ db.js
@@ -0,0 +1,2 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://plarpa:dexter48@cluster0-a6se3.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
