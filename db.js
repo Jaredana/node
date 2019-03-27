62caff81d9e2662075bf64ab938cf5f8f47ca469
Jaredana <jaredana@pdx.edu>
2019-03-27 13:18:43 -0700

    added error check for db connect

--- db.js
+++ db.js
@@ -1,2 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://plarpa:dexter48@cluster0-a6se3.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://plarpa:dexter48@cluster0-a6se3.mongodb.net/test?retryWrites=true', {useNewUrlParser: true}, function(err, db) {
+    if (err) {
+        console.log('Unable to connect to the server. Please start the server. Error:', err);
+    } else {
+        console.log('Connected to Server successfully!');
+    }
+});
